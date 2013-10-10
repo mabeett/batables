b8e9e9ab07618d0daf9f7d287fa0e9d9aac9c5cb
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2013-10-10 17:18:37 -0300

    validates password minimum length and changed de maximum lenth for first name, last name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, :password_confirmation, presence: true, on: :create
+  validates :password, length: { minimum: 6 }, on: :create
   validates :first_name, :last_name, presence: true
-  validates :first_name, :last_name, :email, length: { maximum: 255 }
+  validates :first_name, :last_name, :email, length: { maximum: 50 }
   validates :email,
       uniqueness: { case_sensitive: false },
       presence: true,
