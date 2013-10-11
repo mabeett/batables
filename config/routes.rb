a4d5c388b5ec922d255e79f785210700f4181484
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2013-10-11 14:04:26 -0300

    sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 BaseProject::Application.routes.draw do
+  get "log_in" => "sessions#new", :as => "log_in"
+  post 'sessions', to: 'sessions#create', as: 'sessions'
+  delete "log_out" => "sessions#destroy", :as => "log_out"
+
   resources :users
+
+  root to: 'sessions#new'
 end
