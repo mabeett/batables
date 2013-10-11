8e21e361e56b3e266a6b8bdaf3cdb05189bed935
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2013-10-11 15:32:11 -0300

    authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   private
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize
+    redirect_to log_in_url, alert: 'You must be logged in!' unless current_user
   end
 end
